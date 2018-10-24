36e5756b86a88d06adf6750704000a74cdf2929e
Jeremy Lenz <jeremylenz@gmail.com>
2018-10-23 22:21:48 -0400

    styling wip

--- src/components/BusRouteInfo.js
+++ src/components/BusRouteInfo.js
@@ -5,17 +5,18 @@ const StyledDiv = styled.div`
   text-align: left;
   text-size: 4em;
   font-family: sans-serif;
+  float: right;
 `
 
 const BusRouteInfo = (props) => {
   const { routeDirection, stopNum, stopName } = props
 
   return (
-    <div className='bus-route-info'>
+    <StyledDiv className='bus-route-info'>
       <p>Toward {routeDirection}</p>
       <p>Stop # {stopNum}</p>
       <p>{stopName}</p>
-    </div>
+    </StyledDiv>
   )
 }
 
--- src/components/BusRouteName.js
+++ src/components/BusRouteName.js
@@ -4,17 +4,24 @@ import styled from 'styled-components'
 const StyledSpan = styled.span`
   height: 150px;
   width: 150px;
-  background-color: #bbb;
+  background-color: #b159aa;
   border-radius: 50%;
-  border: 3px;
+  border: 3px black;
+  border-style: solid;
   display: inline-block;
   text-align: center;
-  text-size: 12em;
+  font-size: 4em;
+`
+
+const StyledDiv = styled.div`
+  padding-top: 32px;
 `
 
 const BusRouteName = (props) => (
   <StyledSpan className='bus-route-name'>
-    {props.routeName}
+    <StyledDiv classname='bus-route-name-text'>
+      {props.routeName}
+    </StyledDiv>
   </StyledSpan>
 );
 
