ac4d348c368f0bd3ed55f63cde4bf4e259d5553e
Jeremy Lenz <jeremylenz@gmail.com>
2018-10-27 10:21:51 -0400

    Styling of bus route circle

--- src/components/BusRouteName.js
+++ src/components/BusRouteName.js
@@ -2,28 +2,31 @@ import React from 'react';
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const StyledSpan = styled.span`
+const StyledCircle = styled.div`
   height: 150px;
   width: 150px;
   background-color: #b159aa;
   border-radius: 50%;
   border: 3px black;
   border-style: solid;
-  display: inline-block;
+  display: flex;
   text-align: center;
-  font-size: 2em;
+  font-size: 2.5em;
+  color: white;
+  align-items: center;
+  justify-content: center;
 `
 
 const StyledDiv = styled.div`
-  padding-top: 32px;
+  text-shadow: 1px 1px 1px #000000;
 `
 
 const BusRouteName = (props) => (
-  <StyledSpan className='bus-route-name'>
+  <StyledCircle className='bus-route-name'>
     <StyledDiv classname='bus-route-name-text'>
       <Link to={`/buses/${props.routeName}`}>{props.routeName}</Link>
     </StyledDiv>
-  </StyledSpan>
+  </StyledCircle>
 );
 
 export default BusRouteName;
