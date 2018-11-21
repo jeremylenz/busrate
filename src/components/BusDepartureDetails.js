f2e0b400a21ac00fea43dedf22e901c00fd2bbad
Jeremy Lenz <jeremylenz@gmail.com>
2018-11-21 09:07:41 -0500

    Split BusDepartureDetails into two boxes

--- src/components/BusDepartureDetails.js
+++ src/components/BusDepartureDetails.js
@@ -7,6 +7,7 @@ const StyledDiv = styled.div`
   border-style: solid;
   padding-left: 8px;
   padding-right: 8px;
+  margin-bottom: 8px;
   text-align: left;
   font-size: 1.1em;
 `
@@ -14,17 +15,21 @@ const StyledDiv = styled.div`
 const BusDepartureDetails = (props) => {
   const { stopsAway, minutesAway, progressStatus, recents, yesterday, yesterdayLabel } = props
   return (
-    <StyledDiv className='bus-departure-details'>
-      <p>Expected departure: {minutesAway}</p>
-      <p>{stopsAway}</p>
-      {progressStatus &&
-        <p>{progressStatus}</p>
-      }
-      <p>Recent departures (actual): </p>
-      <p>{recents.join(', ')}</p>
-      <p>{yesterdayLabel}:</p>
-      <p>{yesterday.join(', ')}</p>
-    </StyledDiv>
+    <>
+      <StyledDiv className='bus-departure-details'>
+        <p>Expected departure: {minutesAway}</p>
+        <p>{stopsAway}</p>
+        {progressStatus &&
+          <p>{progressStatus}</p>
+        }
+      </StyledDiv>
+      <StyledDiv>
+        <p>Recent departures (actual): </p>
+        <p>{recents.join(', ')}</p>
+        <p>{yesterdayLabel}:</p>
+        <p>{yesterday.join(', ')}</p>
+      </StyledDiv>
+    </>
  )};
 
 export default BusDepartureDetails;
