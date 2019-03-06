844e21c246fab550dabb730f09c50a3f459d89a6
Jeremy Lenz <jeremylenz@gmail.com>
2019-03-06 14:44:50 -0500

    mock busRoutes

--- src/components/BusRouteSearchPage.test.js
+++ src/components/BusRouteSearchPage.test.js
@@ -5,41 +5,17 @@ import { store } from '../redux/store.js'
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
 import App from '../App.js'
+import { mockedBusRoutes } from '../setupTests.js'
 
 import fetchMock from 'fetch-mock';
 import { LIST_OF_MTA_BUS_ROUTES_URL } from '../constants'
-
-const mockedBusRoutes =  [
-  {
-    "agencyId": "MTABC",
-    "color": "00933C",
-    "description": "Via 188Th St / Union Turnpike",
-    "id": "MTABC_QM31",
-    "longName": "Fresh Meadows - Midtown Via 3 Av",
-    "shortName": "QM31",
-    "textColor": "FFFFFF",
-    "type": 3,
-    "url": "http://web.mta.info/busco/schedules/qm001cur.pdf"
-  },
-  {
-    "agencyId": "MTABC",
-    "color": "00933C",
-    "description": "Via Cross Island Pkwy / Whitestone Expwy",
-    "id": "MTABC_QM32",
-    "longName": "Bay Terrace - Midtown Via 3 Av",
-    "shortName": "QM32",
-    "textColor": "FFFFFF",
-    "type": 3,
-    "url": "http://web.mta.info/busco/schedules/qm002cur.pdf"
-  },
-]
 
 
 it('renders without crashing', () => {
   shallow(<BusRouteSearchPage store={store} />);
 });
 
-it('calls an API', () => {
+it('calls the API to retrieve busRoutes', () => {
 
   // Have fetch return the mocked list of bus routes
   fetchMock.get(LIST_OF_MTA_BUS_ROUTES_URL, mockedBusRoutes);
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,28 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+export const mockedBusRoutes =  [
+  {
+    "agencyId": "MTABC",
+    "color": "00933C",
+    "description": "Via 188Th St / Union Turnpike",
+    "id": "MTABC_QM31",
+    "longName": "Fresh Meadows - Midtown Via 3 Av",
+    "shortName": "QM31",
+    "textColor": "FFFFFF",
+    "type": 3,
+    "url": "http://web.mta.info/busco/schedules/qm001cur.pdf"
+  },
+  {
+    "agencyId": "MTABC",
+    "color": "00933C",
+    "description": "Via Cross Island Pkwy / Whitestone Expwy",
+    "id": "MTABC_QM32",
+    "longName": "Bay Terrace - Midtown Via 3 Av",
+    "shortName": "QM32",
+    "textColor": "FFFFFF",
+    "type": 3,
+    "url": "http://web.mta.info/busco/schedules/qm002cur.pdf"
+  },
+]
