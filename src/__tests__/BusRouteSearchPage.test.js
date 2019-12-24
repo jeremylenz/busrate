584ad47f2389ac429a27cd9911dc04569b88f905
Jeremy Lenz <jeremylenz@gmail.com>
2019-12-24 12:15:19 -0800

    add search test

--- src/__tests__/BusRouteSearchPage.test.js
+++ src/__tests__/BusRouteSearchPage.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-// import { prettyDOM } from '@testing-library/react';
+import { fireEvent, waitForDomChange } from '@testing-library/react';
 import BusRouteSearchPage from '../components/BusRouteSearchPage';
 import { renderWithReduxAndRouter, setWindowWidth } from './testHelpers';
-import { MemoryRouter } from 'react-router-dom';
+// import { MemoryRouter } from 'react-router-dom';
 
 const initialState = {
   busRoutes: { items: [] },
@@ -12,8 +12,10 @@ const initialState = {
 
 // jest.mock('../redux/middleware/core/api.js');
 
+beforeEach(() => setWindowWidth(1024));
+
 test('displays search page', async () => {
-  const { findByPlaceholderText } = renderWithReduxAndRouter(<BusRouteSearchPage />, { initialState, wrapper: MemoryRouter });
+  const { findByPlaceholderText } = renderWithReduxAndRouter(<BusRouteSearchPage />, { initialState });
   setWindowWidth(1024);
   const inputElement = await findByPlaceholderText('Enter bus route...');
   // console.log(prettyDOM(document.querySelector('body')))
@@ -25,4 +27,16 @@ test('displays shorter placeholder text when viewport is smaller', async () => {
   const { queryByPlaceholderText } = renderWithReduxAndRouter(<BusRouteSearchPage />, { initialState });
   const inputElement = queryByPlaceholderText('Enter bus route...');
   expect(inputElement).not.toBeInTheDocument();
+});
+
+test('displays search results as you type', async () => {
+  console.log(window.innerWidth);
+  const { findByPlaceholderText, queryByText } = renderWithReduxAndRouter(<BusRouteSearchPage />, { initialState });
+  const inputElement = await findByPlaceholderText('Enter bus route...');
+
+  fireEvent.click(inputElement);
+  fireEvent.change(inputElement, { target: { value: 'm60' } });
+  await waitForDomChange();
+  const searchResult = queryByText(content => content.startsWith('M60'));
+  expect(searchResult).toBeInTheDocument();
 });
