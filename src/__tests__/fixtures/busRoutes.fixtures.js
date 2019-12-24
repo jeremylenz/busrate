9c803309e51eb9120354dae7d5cee570581be278
Jeremy Lenz <jeremylenz@gmail.com>
2019-12-24 12:32:59 -0800

    add busRoutes fixtures

---
+++ src/__tests__/fixtures/busRoutes.fixtures.js
@@ -0,0 +1,83 @@
+export const busRoutes = [{
+  'agencyId': 'MTA NYCT',
+  'color': 'EE352E',
+  'description': 'Select Bus Service via 125th St / Astoria Blvd',
+  'id': 'MTA NYCT_M60+',
+  'longName': 'LaGuardia Airport - West Side',
+  'shortName': 'M60-SBS',
+  'textColor': 'FFFFFF',
+  'type': 3,
+  'url': ''
+}, {
+  'agencyId': 'MTABC',
+  'color': 'B933AD',
+  'description': 'Via Forest Av / 58Th St / 48Th Av',
+  'id': 'MTABC_Q39',
+  'longName': '48 Ave - 58 St - Forest Ave',
+  'shortName': 'Q39',
+  'textColor': 'FFFFFF',
+  'type': 3,
+  'url': 'http://web.mta.info/busco/schedules/q039cur.pdf'
+}, {
+  'agencyId': 'MTA NYCT',
+  'color': '006CB7',
+  'description': 'Select Bus Service via 1st Av / 2nd Av',
+  'id': 'MTA NYCT_M15+',
+  'longName': 'East Harlem - South Ferry',
+  'shortName': 'M15-SBS',
+  'textColor': 'FFFFFF',
+  'type': 3,
+  'url': ''
+}, {
+  'agencyId': 'MTA NYCT',
+  'color': 'FAA61A',
+  'description': 'via Bloomingdale Rd / Annadale Rd / Richmond Av',
+  'id': 'MTA NYCT_S55',
+  'longName': 'Rossville - Staten Island Mall',
+  'shortName': 'S55',
+  'textColor': 'FFFFFF',
+  'type': 3,
+  'url': ''
+}, {
+  'agencyId': 'MTA NYCT',
+  'color': 'EE352E',
+  'description': 'Select Bus Service via East Gun Hill Rd/White Plains Rd',
+  'id': 'MTA NYCT_BX41+',
+  'longName': 'Williamsbridge - The Hub',
+  'shortName': 'Bx41-SBS',
+  'textColor': 'FFFFFF',
+  'type': 3,
+  'url': ''
+}, {
+  'agencyId': 'MTA NYCT',
+  'color': '006CB7',
+  'description': 'via Riverdale Av / W 231st St / Jerome Av',
+  'id': 'MTA NYCT_BX10',
+  'longName': 'Riverdale - Norwood',
+  'shortName': 'Bx10',
+  'textColor': 'FFFFFF',
+  'type': 3,
+  'url': ''
+}, {
+  'agencyId': 'MTA NYCT',
+  'color': '6CBE45',
+  'description': 'via Bay Ridge Av / Bath Av',
+  'id': 'MTA NYCT_B64',
+  'longName': 'Bay Ridge - Coney Island',
+  'shortName': 'B64',
+  'textColor': 'FFFFFF',
+  'type': 3,
+  'url': ''
+}, {
+  'agencyId': 'MTA NYCT',
+  'color': '00933C',
+  'description': 'via Arthur Kill Rd / Richmond Av',
+  'id': 'MTA NYCT_SIM22',
+  'longName': 'Eltingville - Midtown Manhattan Express',
+  'shortName': 'SIM22',
+  'textColor': 'FFFFFF',
+  'type': 3,
+  'url': ''
+},
+
+];
